aa2c461f2333ba1a872aa9a7bf2f6432b55aa9b1
Steven Huynh <wanderer98@gmail.com>
2023-01-02 04:15:50 -0700

    Fixed font size on mobile

--- wp-content/themes/cafe-jindo/build/index.asset.php
+++ wp-content/themes/cafe-jindo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '66327eba2366eee4e94d2f91e9697756');
+<?php return array('dependencies' => array(), 'version' => '54217692b01bb86628c5bc90eb248cb9');
